7b170f95f2435f78a540112068ae38318be7ae15
h2magic <h2magic@126.com>
2022-09-03 20:02:23 +0800

    .

---
+++ lib/pages/body/TaskPanel/task_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class TaskDataController extends GetxController {
+
+}
---
+++ lib/pages/body/TaskPanel/task_panel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_management/pages/body/TaskPanel/task_controller.dart';
+
+class TaskPanel extends StatelessWidget {
+  TaskPanel({Key? key}) : super(key: key);
+
+  final taskDataController = TaskDataController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
